a925f65d61a67220bcf246f91b0c4cb1d5b69bf0
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-08-07 22:36:21 -0500

    Added Mining rigs and Energy management

--- app.js
+++ app.js
@@ -163,9 +163,6 @@ const cryptocurrencies = [
 let marketInfoModal = document.getElementById('market-info-modal');
 let marketInfoBtn = document.getElementById('show-market-info-btn');
 let closeMarketInfo = document.getElementsByClassName('close')[0];
-
-
-
 // End of Market Value
 
 // Unified Modal Logic for all modals
@@ -188,8 +185,241 @@ document.getElementById('show-market-info-btn').addEventListener('click', functi
     displayMarketValues();
 });
 
+document.getElementById('show-shop-btn').addEventListener('click', function() {
+    showModal('shop-modal');
+    displayMarketValues();
+});
+
 window.onclick = function(event) {
     if (event.target.classList.contains('modal')) {
         hideModal(event.target.id);
     }
 }
+
+
+// Create Mining Rigs
+
+const miningRigs = [
+    {
+        name: 'Zeus Miner',
+        miningSpeed: 50,
+        energyConsumption: 20,
+        cost: 10000,
+        breakdownProbability: 0.02,
+        repairCost: 800,
+        heatGeneration: 15,
+        resaleValue: 7000,
+        upgradeSlots: 4
+    },
+    {
+        name: 'Hera Excavator',
+        miningSpeed: 45,
+        energyConsumption: 18,
+        cost: 9000,
+        breakdownProbability: 0.018,
+        repairCost: 750,
+        heatGeneration: 14,
+        resaleValue: 6300,
+        upgradeSlots: 3
+    },
+    {
+        name: 'Poseidon Drill',
+        miningSpeed: 40,
+        energyConsumption: 16,
+        cost: 8000,
+        breakdownProbability: 0.016,
+        repairCost: 700,
+        heatGeneration: 13,
+        resaleValue: 5600,
+        upgradeSlots: 3
+    },
+    {
+        name: 'Athena Processor',
+        miningSpeed: 35,
+        energyConsumption: 14,
+        cost: 7000,
+        breakdownProbability: 0.015,
+        repairCost: 650,
+        heatGeneration: 12,
+        resaleValue: 4900,
+        upgradeSlots: 2
+    },
+    {
+        name: 'Ares Harvester',
+        miningSpeed: 30,
+        energyConsumption: 12,
+        cost: 6000,
+        breakdownProbability: 0.014,
+        repairCost: 600,
+        heatGeneration: 11,
+        resaleValue: 4200,
+        upgradeSlots: 2
+    },
+    {
+        name: 'Apollo Rig',
+        miningSpeed: 25,
+        energyConsumption: 10,
+        cost: 5000,
+        breakdownProbability: 0.012,
+        repairCost: 550,
+        heatGeneration: 10,
+        resaleValue: 3500,
+        upgradeSlots: 1
+    },
+    {
+        name: 'Artemis Extractor',
+        miningSpeed: 20,
+        energyConsumption: 8,
+        cost: 4000,
+        breakdownProbability: 0.01,
+        repairCost: 500,
+        heatGeneration: 9,
+        resaleValue: 2800,
+        upgradeSlots: 1
+    },
+    {
+        name: 'Hermes Miner',
+        miningSpeed: 15,
+        energyConsumption: 6,
+        cost: 3000,
+        breakdownProbability: 0.008,
+        repairCost: 450,
+        heatGeneration: 8,
+        resaleValue: 2100,
+        upgradeSlots: 0
+    },
+    {
+        name: 'Hephaestus Furnace',
+        miningSpeed: 10,
+        energyConsumption: 4,
+        cost: 2000,
+        breakdownProbability: 0.006,
+        repairCost: 400,
+        heatGeneration: 7,
+        resaleValue: 1400,
+        upgradeSlots: 0
+    },
+    {
+        name: 'Demeter Digger',
+        miningSpeed: 5,
+        energyConsumption: 2,
+        cost: 1000,
+        breakdownProbability: 0.004,
+        repairCost: 300,
+        heatGeneration: 6,
+        resaleValue: 700,
+        upgradeSlots: 0
+    }
+];
+
+let shopModal = document.getElementById('shop-modal');
+let shopBtn = document.getElementById('show-shop-btn');
+let closeShop = shopModal.querySelector('.close');
+
+function showMiningRigs() {
+    const rigList = document.getElementById('mining-rigs-list');
+    rigList.innerHTML = '';
+
+    miningRigs.forEach(rig => {
+        const rigInfo = document.createElement('div');
+        rigInfo.innerHTML = `
+            <h3>${rig.name}</h3>
+            <p>Mining Speed: ${rig.miningSpeed}</p>
+            <p>Energy Consumption: ${rig.energyConsumption}</p>
+            <p>Cost: $${rig.cost}</p>
+            <p>Breakdown Probability: ${(rig.breakdownProbability * 100)}% per hour</p>
+            <p>Repair Cost: $${rig.repairCost}</p>
+            <p>Heat Generation: ${rig.heatGeneration}</p>
+            <p>Resale Value: $${rig.resaleValue}</p>
+            <p>Upgrade Slots: ${rig.upgradeSlots}</p>
+            <button>Buy</button>
+        `;
+        rigList.appendChild(rigInfo);
+    });
+}
+
+
+// Energy management
+const energyUnits = [
+    {
+        name: "Hestia's Hearth",
+        powerOutput: 100,
+        cost: 1000,
+        description: "Harness the divine hearth's energy for basic power needs."
+    },
+    {
+        name: "Helios' Radiance",
+        powerOutput: 200,
+        cost: 2000,
+        description: "Utilize the sun god's brilliance for a consistent power supply."
+    },
+    {
+        name: "Hermes' Dynamo",
+        powerOutput: 300,
+        cost: 3000,
+        description: "Leverage the messenger god's speed for dynamic power generation."
+    },
+    {
+        name: "Demeter's Bounty",
+        powerOutput: 400,
+        cost: 4000,
+        description: "Use the harvest goddess's riches for abundant power production."
+    },
+    {
+        name: "Poseidon's Surge",
+        powerOutput: 500,
+        cost: 5000,
+        description: "Employ the sea god's tidal force for a high-energy power boost."
+    },
+    {
+        name: "Aeolus' Gale",
+        powerOutput: 600,
+        cost: 6000,
+        description: "Use the wind lord's gusts for efficient wind power generation."
+    },
+    {
+        name: "Hephaestus' Forge",
+        powerOutput: 700,
+        cost: 7000,
+        description: "Harness the blacksmith god's fiery forge for high-temperature power production."
+    },
+    {
+        name: "Apollo's Beam",
+        powerOutput: 800,
+        cost: 8000,
+        description: "Utilize the sun god's piercing light for powerful solar energy generation."
+    },
+    {
+        name: "Zeus' Bolt",
+        powerOutput: 900,
+        cost: 9000,
+        description: "Employ the king god's thunderbolt for electrifying power supply."
+    },
+    {
+        name: "Gaia's Pulse",
+        powerOutput: 1000,
+        cost: 10000,
+        description: "Leverage the earth mother's life force for sustainable geothermal energy."
+    }
+];
+
+
+
+function showEnergyManagement() {
+    const energyList = document.getElementById('energy-management-list');
+    energyList.innerHTML = '';
+
+    energyUnits.forEach(unit => {
+        const unitInfo = document.createElement('div');
+        unitInfo.innerHTML = `
+            <h3>${unit.name}</h3>
+            <p>Power Output: ${unit.powerOutput}</p>
+            <p>Cost: $${unit.cost}</p>
+            <p>Description: ${unit.description}</p>
+            <button>Buy</button>
+        `;
+        energyList.appendChild(unitInfo);
+    });
+}
+
+
